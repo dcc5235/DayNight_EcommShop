99cda8ee2bf7f4ac85133beb072bae4e9b633ca1
Dany <dany.chheang@gmail.com>
2021-03-19 10:10:39 -0500

    updated menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
+const seasonal = () => (
+  <div>
+    <h1>other Page</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/seasonal' component={seasonal} />
+      </Switch>
     </div>
   );
 }
