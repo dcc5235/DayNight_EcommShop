5b8dfecc0960a8a507e947f04d3401930d04510f
Dany <dany.chheang@gmail.com>
2021-03-23 13:15:48 -0500

    added shop data with pricing, title, and images

---
+++ src/pages/shop/shop.data.js
@@ -0,0 +1,263 @@
+const SHOP_DATA = [
+  {
+    id: 1,
+    title: 'Easter Collection',
+    routeName: 'easter',
+    items: [
+      {
+        id: 1,
+        name: 'Cora Dessert Dish Set',
+        imageUrl: 'https://images.pexels.com/photos/4110001/pexels-photo-4110001.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 48.95
+      },
+
+      {
+        id: 2,
+        name: 'Agath Wooden Flatware',
+        imageUrl: 'https://images.pexels.com/photos/4397822/pexels-photo-4397822.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 34.95
+      },
+
+      {
+        id: 3,
+        name: 'Acacia Serving Board',
+        imageUrl: 'https://images.pexels.com/photos/4087609/pexels-photo-4087609.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 39.95
+      },
+      {
+        id: 4,
+        name: 'Sicil Wood Platter',
+        imageUrl: 'https://images.pexels.com/photos/2280620/pexels-photo-2280620.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 29.95
+      },
+      {
+        id: 5,
+        name: 'Esther 3-Piece Table Setting',
+        // imageUrl: '/images/shop-img/easter/tapir-holder.jpg',
+        imageUrl: 'https://images.pexels.com/photos/5591663/pexels-photo-5591663.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 42.95
+      },
+      {
+        id: 6,
+        name: 'Pomodore Wood Serving Board',
+        imageUrl: 'https://images.pexels.com/photos/5460885/pexels-photo-5460885.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        // imageUrl: "/images/shop-img/hats/green-beanie.png",
+        price: 39.95
+      },
+      {
+        id: 7,
+        name: 'Mateo Wood Serving Board',
+        imageUrl: 'https://images.pexels.com/photos/5107183/pexels-photo-5107183.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        // imageUrl: "/images/shop-img/hats/red-beanie.png",
+        price: 18
+      },
+      {
+        id: 8,
+        name: 'Rustic Dessert Set',
+        imageUrl: 'https://images.pexels.com/photos/3646863/pexels-photo-3646863.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        // imageUrl: "/images/shop-img/hats/wolf-cap.png",
+        price: 29.95
+      },
+      {
+        id: 9,
+        name: 'Colier Glassware',
+        imageUrl: 'https://images.pexels.com/photos/5107162/pexels-photo-5107162.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        // imageUrl: "/images/shop-img/hats/blue-beanie.png",
+        price: 9.95
+      },
+
+    ]
+  },
+  {
+    id: 2,
+    title: 'Kitchen',
+    routeName: 'kitchen',
+    items: [
+      {
+        id: 10,
+        name: 'Rose Gold Whisk',
+        imageUrl: 'https://images.pexels.com/photos/6569306/pexels-photo-6569306.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 9.95
+      },
+      {
+        id: 11,
+        name: 'Glass Mixing Bowl Set',
+        imageUrl: 'https://images.pexels.com/photos/5908033/pexels-photo-5908033.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 29.95
+      },
+
+      {
+        id: 12,
+        name: 'Dove Mixing Bowl',
+        imageUrl: 'https://images.pexels.com/photos/4110237/pexels-photo-4110237.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        price: 39.95
+      },
+      {
+        id: 13,
+        name: 'Turquoise Cupcake Pan',
+        imageUrl: 'https://images.pexels.com/photos/7123131/pexels-photo-7123131.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 14.95
+      },
+      {
+        id: 14,
+        name: 'Egg Yolk Separator',
+        imageUrl: 'https://images.pexels.com/photos/4911765/pexels-photo-4911765.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 8.95
+      },
+      {
+        id: 15,
+        name: 'Cuisinè Pasta Roller',
+        imageUrl: 'https://images.pexels.com/photos/6287334/pexels-photo-6287334.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        price: 79.95
+      },
+      {
+        id: 16,
+        name: 'Kitchenpro Artisan Hand Mixer',
+        imageUrl: 'https://images.pexels.com/photos/5908003/pexels-photo-5908003.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 39.95
+      },
+      {
+        id: 17,
+        name: 'Wooden Mixing Spoon',
+        imageUrl: 'https://images.pexels.com/photos/7123144/pexels-photo-7123144.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        price: 4.95
+      }
+    ]
+  },
+  {
+    id: 3,
+    title: 'Decorative Objects',
+    routeName: 'decor',
+    items: [
+      {
+        id: 18,
+        name: 'Ceramic Tabletop Sculpture',
+        imageUrl: 'https://images.pexels.com/photos/4203098/pexels-photo-4203098.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 69.95
+      },
+      {
+        id: 19,
+        name: 'Ceramic Tabletop Vase',
+        imageUrl: 'https://images.pexels.com/photos/4207791/pexels-photo-4207791.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 49.95
+      },
+
+      {
+        id: 20,
+        name: 'Blane Hourglass',
+        imageUrl: 'https://images.pexels.com/photos/4397902/pexels-photo-4397902.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 39.95
+      },
+
+      {
+        id: 21,
+        name: 'Marbel Bookends, Set of 2',
+        imageUrl: 'https://images.pexels.com/photos/4207789/pexels-photo-4207789.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 49.95
+      },
+
+      {
+        id: 22,
+        name: 'Find Joy Quote ',
+        imageUrl: 'https://images.pexels.com/photos/2082091/pexels-photo-2082091.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 16.95
+      },
+    ]
+  },
+  {
+    id: 4,
+    title: 'Furniture',
+    routeName: 'furniture',
+    items: [
+      {
+        id: 23,
+        name: 'Atlantic Lounge Sofa',
+        imageUrl: 'https://images.pexels.com/photos/4352247/pexels-photo-4352247.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 1799.99
+      },
+      {
+        id: 24,
+        name: 'Calantra Wood End Table',
+        imageUrl: 'https://images.pexels.com/photos/6266189/pexels-photo-6266189.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 699.99
+      },
+      {
+        id: 25,
+        name: 'Solovan Office Shelf',
+        imageUrl: 'https://images.pexels.com/photos/4083654/pexels-photo-4083654.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 89.99
+      },
+      {
+        id: 26,
+        name: 'Neal Natural Bed',
+        imageUrl: 'https://images.pexels.com/photos/545012/pexels-photo-545012.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 1499.99
+      },
+      {
+        id: 27,
+        name: 'Doris High-Backed Chair',
+        imageUrl: 'https://images.pexels.com/photos/6707628/pexels-photo-6707628.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 679.99
+      },
+      {
+        id: 28,
+        name: 'Delaney Wicker Chair',
+        imageUrl: 'https://images.pexels.com/photos/4992477/pexels-photo-4992477.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 1299.99
+      },
+      {
+        id: 29,
+        name: 'Geo Wood Dresser',
+        imageUrl: 'https://images.pexels.com/photos/5997975/pexels-photo-5997975.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 599.99
+      }
+    ]
+  },
+  {
+    id: 5,
+    title: 'Vases and Botanicals',
+    routeName: 'mens',
+    items: [
+      {
+        id: 30,
+        name: 'Dol Planter with Artificial Potted Plant',
+        imageUrl: 'https://images.pexels.com/photos/4363420/pexels-photo-4363420.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 99.95
+      },
+      {
+        id: 31,
+        name: 'Palm Paper Leaves, Set of 3',
+        imageUrl: 'https://images.pexels.com/photos/6707632/pexels-photo-6707632.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 19.95
+      },
+      {
+        id: 32,
+        name: 'Gourd Vases - Set of 2',
+        imageUrl: 'https://images.pexels.com/photos/2789545/pexels-photo-2789545.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 49.95
+      },
+
+      {
+        id: 33,
+        name: 'Alan Cermaic Vase',
+        imageUrl: 'https://images.pexels.com/photos/2746155/pexels-photo-2746155.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 44.95
+      },
+      {
+        id: 33,
+        name: 'Green Paper Palms, Set of 3',
+        imageUrl: 'https://images.pexels.com/photos/4503828/pexels-photo-4503828.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 25.95
+      },
+      {
+        id: 34,
+        name: 'Tall Black Vase',
+        imageUrl: 'https://images.pexels.com/photos/4846094/pexels-photo-4846094.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 99.95
+      },
+
+    ]
+  }
+];
+
+export default SHOP_DATA;
