38a301aab8c672fe330f685c79aa35266c0bab14
Dany <dany.chheang@gmail.com>
2021-04-08 13:52:37 -0500

    included method to store user data on sign in

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import SignInSignUpPage from './pages/signin-signup/signin-signup.component.jsx';
 import Header from './components/header/header.component.jsx';
-import { auth } from './firebase/firebase.utils';
-import { render } from '@testing-library/react';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+// import { render } from '@testing-library/react';
 
 class App extends React.Component {
   constructor() {
@@ -20,8 +20,20 @@ class App extends React.Component {
 
   unsubscribeFromAuth = null
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      }
+      else this.setState({ currentUser: userAuth });
     });
   }
 
