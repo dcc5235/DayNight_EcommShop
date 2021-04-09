ad0c9af00a5a52471af0e49af88c70afb94aaf93
Dany <dany.chheang@gmail.com>
2021-04-09 15:20:03 -0500

    implemented ability to store authenticated users in database with firebase

--- src/pages/signin-signup/signin-signup.component.jsx
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import SignIn from '../../components/signin/signin.component'
+
+import SignIn from '../../components/signin/signin.component';
+import SignUp from '../../components/signup/signup.component';
+
 import './signin-signup.styles.scss';
 
 const SignInSignUpPage = () => (
-  <div className='signin-signup'><SignIn /></div>
+  <div className='signin-signup'><SignIn /><SignUp /></div>
 );
 
 export default SignInSignUpPage;
